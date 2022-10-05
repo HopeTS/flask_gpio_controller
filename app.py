675e69f732446d9422c4755a23860a41392b94c4
Robby Bergers <bergersr97@gmail.com>
2022-10-05 02:49:39 -0400

    fix port

--- app.py
+++ app.py
@@ -181,7 +181,7 @@ if __name__ == "__main__":
             raise
 
         # Run
-        socketio.run(app, config['HTTP_PORT'])
+        socketio.run(app, config['WS_PORT'])
         #app.run(debug=True, port=config['HTTP_PORT'], host="0.0.0.0")
 
     except:
